04b14de621f252a68d4b3560aeffe9012837429f
Алексей Зосимов <aleksei.zosimoff@yandex.ru>
2024-05-25 21:37:43 +0300

    first_try

---
+++ delivery_sprint_4.py
@@ -0,0 +1,33 @@
+import sys
+
+
+def platforms_counter(robots_masses: list, max_weight: int) -> int:
+    left: int = 0
+    right: int = len(robots_masses) - 1
+    counter: int = 0
+
+    while left <= right:
+        if left == right:
+            counter += 1
+            break
+        elif (left < right and
+              robots_masses[left] + robots_masses[right] <= max_weight):
+            counter += 1
+            left += 1
+            right -= 1
+        elif (left < right and
+              robots_masses[left] + robots_masses[right] > max_weight):
+            counter += 1
+            right -= 1
+    return counter
+
+
+def main():
+    robots_masses = [int(i) for i in sys.stdin.readline().strip().split()]
+    robots_masses = sorted(robots_masses)
+    max_weight = int(sys.stdin.readline().strip())
+    print(platforms_counter(robots_masses, max_weight))
+
+
+if __name__ == '__main__':
+    main()
